4a0f594f9b20215b4667dcc28a7819ffbec1e570
Tiffany Chen <loveseo1124@gmail.com>
2019-12-09 02:28:49 +0800

    test flex json

--- line.py
+++ line.py
@@ -287,7 +287,6 @@ def handle_message(event):
                 }
             }
         )
-        line_bot_api.reply_message(event.reply_token, flex_message)
 
         buttons_template_message = TemplateSendMessage(
             alt_text='Buttons template',
@@ -319,7 +318,7 @@ def handle_message(event):
                 ]
             )
         )
-        line_bot_api.reply_message(event.reply_token, buttons_template_message)
+        line_bot_api.reply_message(event.reply_token, [flex_message, buttons_template_message])
         
     elif user_input == 'no':
         line_bot_api.reply_message(event.reply_token, TextSendMessage(text='no'))
