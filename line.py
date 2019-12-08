84c77c2f7cbcfeec819d22427d259180b4d6a053
Tiffany Chen <loveseo1124@gmail.com>
2019-12-08 12:18:14 +0800

    modify highcharts

--- line.py
+++ line.py
@@ -286,7 +286,39 @@ def handle_postback(event):
 
 @app.route("/charts", methods=['GET'])
 def show_charts():
-    return render_template('charts.html')
+    categories = ['北部', '中部', '南部']
+    north = {
+        'name': '北部',
+        'data': [10],
+        'color': 'green',
+    }
+
+    central = {
+        'name': '中部',
+        'data': [20],
+        'color': 'red',
+    }
+
+    south = {
+        'name': '南部',
+        'data': [5],
+        'color': 'blue',
+    }
+
+    chart = {
+        'chart': {'type': 'column'},
+        'title': {'text': '會員居住地分布'},
+        'xAxis': {'categories': categories},
+        'series': [north, central, south],
+        'plotOptions': {
+            'series': {
+                'grouping': False,
+            }
+        }
+    }
+
+    dump = json.dumps(chart)
+    return render_template('charts.html', chart=dump)
 
 
 if __name__ == "__main__":
