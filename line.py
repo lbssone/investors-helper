0f1dd97b87f4e0da3128446e1a205e6d71514839
Tiffany Chen <loveseo1124@gmail.com>
2019-12-19 12:55:22 +0800

    revise code

--- line.py
+++ line.py
@@ -12,7 +12,7 @@ from linebot.exceptions import (
 )
 from linebot.models import *
 
-import accounts_contents from './src/accounts'
+from src.accounts import accounts_contents  
 
 
 app = Flask(__name__)
