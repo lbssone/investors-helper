49f22fb3215c929175734db3c775497d27703321
Tiffany Chen <loveseo1124@gmail.com>
2019-12-22 16:47:30 +0800

    test notify price

--- line.py
+++ line.py
@@ -12,7 +12,7 @@ from linebot.exceptions import (
 from linebot.models import *
 
 from src.accounts_contents import accounts_contents  
-from twstock import *
+import twstock
 import time
 import datetime
 
